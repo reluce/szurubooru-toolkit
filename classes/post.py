96f7d6af54669780d30fa0c13198e2ca5c0867ad
reluce <armin@richter-nt.de>
2021-05-01 15:06:40 +0200

    wip

--- classes/post.py
+++ classes/post.py
@@ -1,5 +1,5 @@
 class Post:
-    def __init__(self, id=None, image_url=None, version=None, tags=[]):
+    def __init__(self, md5sum, id=None, image_url=None, version=None, tags=[]):
         self.id            = id
         self.version       = version
         self.image_url     = image_url
@@ -9,23 +9,5 @@ class Post:
         self.rating        = 'unsafe'
         self.source        = 'Anonymous'
         self.exact_post    = None
+        self.md5sum        = md5sum
         self.similar_posts = []
-
-    def describe(self):
-        """
-        Prints the currently assigned attributes of the object.
-        """
-
-        data = {
-            'id': self.id,
-            'version': self.version,
-            'image_url': self.image_url,
-            'tags': self.tags,
-            'rating': self.rating,
-            'source': self.source,
-            'image_token': self.image_token,
-            'exact_post': self.exact_post,
-            'similar_posts': self.similar_posts,
-        }
-
-        print(data)
