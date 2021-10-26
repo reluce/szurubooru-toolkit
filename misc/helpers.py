77c535ea72db657b478278018c1460098c40e65a
Manicarus <swkang7229@gmail.com>
2021-10-26 12:04:20 +0900

    Added frequently used functions while scraping

--- misc/helpers.py
+++ misc/helpers.py
@@ -54,3 +54,101 @@ def statistics(tagged=0, untagged=0):
 
     return total_tagged, total_untagged
 
+def audit_rating(*ratings):
+    '''
+    Return the highest among the scraped ratings
+    Returns 'safe' if ratings is an empty list
+    '''
+
+    verdict = 'safe'
+    weight = {
+        'unsafe' : 2,
+        'sketchy': 1,
+        'safe'   : 0
+    }
+    for r in ratings:
+        if not r:
+            continue
+        if weight[r] > weight[verdict]:
+            verdict = r
+    return verdict
+
+def check_similarity(result, cutline):
+    '''
+    Allow results that are more similar than configured cutline
+    '''
+
+    is_similar = False
+    if result.similarity > cutline:
+        is_similar = True
+    return is_similar
+
+def remove_tag_whitespace(tags_whitespace):
+    '''
+    Self descriptive
+    Szurubooru does not allow whitespaces in tagnames
+
+    Arguments:
+        tags: list of tags possibly with whitespaces
+
+    Returns:
+        tags_: list of tags with underscores
+    '''
+    tags_underscore = []
+    for tw in tags_whitespace:
+        tu = tw.replace(' ', '_')
+        tags_underscore.append(tu)
+
+    return tags_underscore
+
+def collect_tags(*list_tags):
+    '''
+    Collect tags and remove duplicates
+    Retuns an empty list if tags is an empty list
+
+    Arguments:
+        list_tags: A list of lists of tags
+
+    Returns:
+        tags_collected: self descriptive
+            duplicates are removed
+    '''
+
+    tags_collected = []
+    for lt in list_tags:
+        tu = remove_tag_whitespace(lt)
+        tags_collected.extend(tu)
+
+    for tc in tags_collected:
+        tc.replace(' ', '_')
+
+    # remove duplicates
+    tags_collected = list(set(tags_collected))
+    return tags_collected
+
+def collect_sources(*sources):
+    '''
+    Collect sources in a single string separated by newline characters
+    Returns an empty string if sources is an empty list
+
+    Arguments:
+        sources: A list of source URL strings
+
+    Returns:
+        source_collected: Collection of sources in a string
+            separated by newline character
+    '''
+
+    source_valid = []
+    # remove empty sources
+    for s in sources:
+        if not s:
+            continue
+        source_valid.append(s)
+
+    # remove duplicates
+    source_valid = list(set(source_valid))
+
+    delimiter = '\n'
+    source_collected = delimiter.join(source_valid)
+    return source_collected
