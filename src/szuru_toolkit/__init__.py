98b8b5b05f7e63e90cd1bfe2656f6bafcc064db4
reluce <reluce@fkosquad.moe>
2022-04-02 12:52:45 +0200

    sort imports, use conditional deepbooru import

--- src/szuru_toolkit/__init__.py
+++ src/szuru_toolkit/__init__.py
@@ -1,6 +1,5 @@
 from .config import Config
 from .danbooru import Danbooru
-from .deepbooru import Deepbooru
 from .gelbooru import Gelbooru
 from .sankaku import scrape_sankaku  # noqa F401
 from .saucenao import SauceNao
@@ -16,10 +15,13 @@ from .utils import setup_logger  # noqa F401
 from .utils import statistics  # noqa F401
 
 
+config = Config()
+if config.auto_tagger['deepbooru_enabled']:
+    from .deepbooru import Deepbooru  # noqa F401
+
 __all__ = [
     'Config',
     'Danbooru',
-    'Deepbooru',
     'Gelbooru',
     'Pixiv',
     'Post',
