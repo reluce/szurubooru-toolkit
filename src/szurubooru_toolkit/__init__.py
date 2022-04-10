cd1325017113cc6034afea9e463caad3fb8b1082
reluce <reluce@fkosquad.moe>
2022-04-10 23:03:10 +0200

    remove prolly unused all import

--- src/szurubooru_toolkit/__init__.py
+++ src/szurubooru_toolkit/__init__.py
@@ -3,14 +3,15 @@ import sys
 from loguru import logger
 
 from .config import Config
-from .danbooru import Danbooru
-from .gelbooru import Gelbooru
-from .saucenao import SauceNao
-from .szurubooru import Post
+from .danbooru import Danbooru  # noqa F401
+from .gelbooru import Gelbooru  # noqa F401
+from .saucenao import SauceNao  # noqa F401
+from .szurubooru import Post  # noqa F401
 from .szurubooru import Szurubooru
 from .utils import audit_rating  # noqa F401
 from .utils import collect_sources  # noqa F401
 from .utils import convert_rating  # noqa F401
+from .utils import get_md5sum  # noqa F401
 from .utils import sanitize_tags  # noqa F401
 from .utils import scrape_sankaku  # noqa F401
 from .utils import setup_logger  # noqa F401
@@ -38,14 +39,3 @@ if config.auto_tagger['deepbooru_enabled']:
 setup_logger(config)
 
 szuru = Szurubooru(config.szurubooru['url'], config.szurubooru['username'], config.szurubooru['api_token'])
-
-__all__ = [
-    'Config',
-    'Danbooru',
-    'Gelbooru',
-    'Pixiv',
-    'Post',
-    'Sankaku',
-    'SauceNao',
-    'Szurubooru',
-]
