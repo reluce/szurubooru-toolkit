2bb4d618b2fd097f57cef4a93888c0fee78e5dc8
reluce <reluce@fkosquad.moe>
2022-04-16 15:08:11 +0200

    import import_from_booru

--- src/szurubooru_toolkit/scripts/__init__.py
+++ src/szurubooru_toolkit/scripts/__init__.py
@@ -1,6 +1,7 @@
 from .auto_tagger import main  # noqa F401
 from .create_tags import main  # noqa F401
 from .delete_posts import main  # noqa F401
+from .import_from_booru import main  # noqa F401
 from .reset_posts import main  # noqa F401
 from .tag_posts import main  # noqa F401
 from .upload_media import main  # noqa F401
