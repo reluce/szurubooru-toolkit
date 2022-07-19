3032401610e243a96e22b1ea8550701c5e03ca70
reluce <reluce@fkosquad.moe>
2022-07-19 22:36:48 +0200

    add pagination

--- src/szurubooru_toolkit/twitter.py
+++ src/szurubooru_toolkit/twitter.py
@@ -1,4 +1,5 @@
 import re
+from math import ceil
 
 import tweepy
 from loguru import logger
@@ -39,6 +40,25 @@ class Twitter:
             list: A list which contains the tweet URL and the associated media file URLs as a tuple.
         """
 
+        def set_media_refs(data: list, tweets: list) -> None:
+            """Appends a tuple of the Tweet url and their media ref links to a list.
+
+            Args:
+                data (list): The data list from the Twitter response.
+                tweets (list): List where the tuples will get appended to.
+            """
+            for tweet in data:
+                if tweet.attachments:
+                    tweet_url = Twitter.get_tweet_url(tweet.entities['urls'])
+                    media_refs = Twitter.get_media_refs(tweet.attachments['media_keys'], response.includes['media'])
+                    tweets.append(tuple((tweet_url, media_refs)))
+
+        if limit > 100:
+            total_posts_to_fetch = limit
+            limit = 100
+        else:
+            total_posts_to_fetch = None
+
         response = self.client.get_liked_tweets(
             user_id,
             user_auth=True,
@@ -48,13 +68,41 @@ class Twitter:
             max_results=limit,
         )
 
+        next_token = response.meta['next_token']
         tweets = []
 
-        for tweet in response.data:
-            if tweet.attachments:
-                tweet_url = Twitter.get_tweet_url(tweet.entities['urls'])
-                media_refs = Twitter.get_media_refs(tweet.attachments['media_keys'], response.includes['media'])
-                tweets.append(tuple((tweet_url, media_refs)))
+        set_media_refs(response.data, tweets)
+
+        # If user limit is > 100, start pagination.
+        if total_posts_to_fetch:
+            total_pages = ceil(total_posts_to_fetch / 100)
+            page = 2  # We already retrieved page 1
+            while next_token and page <= total_pages:
+                # On the last page, retrieve only the last double digits posts from the limit.
+                # 1230 -> 30, 123 -> 23
+                # If last two digits are 0s, assume 100 and fetch the max amount of posts.
+                if page == total_pages:
+                    limit = int(str(total_posts_to_fetch)[-2:])
+                    if limit == 0:
+                        limit = 100
+
+                response = self.client.get_liked_tweets(
+                    user_id,
+                    user_auth=True,
+                    expansions=['attachments.media_keys'],
+                    tweet_fields=['entities'],
+                    media_fields=['url', 'variants'],
+                    max_results=limit,
+                    pagination_token=next_token,
+                )
+
+                try:
+                    next_token = response.meta['next_token']
+                except KeyError:  # In case we reached the last page
+                    next_token = False
+
+                page += 1
+                set_media_refs(response.data, tweets)
 
         return tweets
 
