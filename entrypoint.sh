e03bd5f3f420b056fab283681002d9d3fe0c2531
shibao <shibao@bubbletea.dev>
2022-10-23 13:40:26 -0400

    add aliases

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 
+# add aliases
+alias auto-tagger="poetry run auto-tagger"
+alias create-tags="poetry run create-tags"
+alias delete-posts="poetry run delete-posts"
+alias import-from-booru="poetry run import-from-booru"
+alias reset-posts="poetry run reset-posts"
+alias upload-media="poetry run upload-media"
+alias tag-posts="poetry run tag-posts"
+
+# setup crontab
 chown root /etc/cron.d/crontab
 chmod 644 /etc/cron.d/crontab
 crontab /etc/cron.d/crontab
