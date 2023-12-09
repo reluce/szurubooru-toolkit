ccc64b25b1f8dff45243139c0c859b43226f1306
reluce <reluce@fkosquad.moe>
2023-12-09 15:57:15 +0100

    don't lookup pixiv.net/fanbox

--- src/szurubooru_toolkit/pixiv.py
+++ src/szurubooru_toolkit/pixiv.py
@@ -11,34 +11,42 @@ class Pixiv:
         self.client.auth(refresh_token=token)
 
     def get_result(self, result_url):
-        post_id = int(result_url.split('=')[-1])
-        logger.debug(f'Getting result from id {post_id}')
-        for _ in range(1, 12):
-            try:
-                result = self.client.illust_detail(post_id)
-                logger.debug(f'Returning result: {result}')
-                break
-            except ClientConnectorError:
-                logger.debug('Could not establish connection to Pixiv, trying again in 5s...')
-                sleep(5)
-            except KeyError:  # In case the post got deleted but is still indexed
+        if 'pixiv.net/fanbox' not in result_url:
+            post_id = int(result_url.split('=')[-1])
+            logger.debug(f'Getting result from id {post_id}')
+
+            for _ in range(1, 12):
+                try:
+                    result = self.client.illust_detail(post_id)
+                    logger.debug(f'Returning result: {result}')
+                    break
+                except ClientConnectorError:
+                    logger.debug('Could not establish connection to Pixiv, trying again in 5s...')
+                    sleep(5)
+                except KeyError:  # In case the post got deleted but is still indexed
+                    result = None
+                    logger.debug('Got no result')
+                    break
+            else:
                 result = None
-                logger.debug('Got no result')
-                break
         else:
+            # Don't lookup tags for Fanbox as they're paywalled
             result = None
 
         return result
 
     def get_tags(self, result):
         tags = []
+
         if result.illust and result.illust.tags:
             for tag in result.illust.tags:
                 temp = tag['name']
                 if temp is not None:
                     if not temp == 'R-18':
                         tags.append(temp)
+
         logger.debug(f'Returning tags {tags}')
+
         return tags
 
     def get_rating(self, result):
