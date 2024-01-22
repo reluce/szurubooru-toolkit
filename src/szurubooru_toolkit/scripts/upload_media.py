cf1170c663c52fe474611a3bb38b47cffd3d5892
gljames24 <gljames24@gmail.com>
2024-01-21 17:19:32 -0700

    Append tags if exact duplicate is found

--- src/szurubooru_toolkit/scripts/upload_media.py
+++ src/szurubooru_toolkit/scripts/upload_media.py
@@ -5,6 +5,7 @@ import os
 import shutil
 from glob import glob
 from pathlib import Path
+import subprocess
 
 import requests
 from loguru import logger
@@ -19,11 +20,6 @@ from szurubooru_toolkit.scripts.auto_tagger import main as auto_tagger
 
 
 szuru = Szurubooru(config.szurubooru['url'], config.szurubooru['username'], config.szurubooru['api_token'])
-
-def get_image_resolution(image_bytes: bytes) -> tuple:
-    """Get the resolution of an image."""
-    with Image.open(io.BytesIO(image_bytes)) as img:
-        return img.size
 
 def get_files(upload_dir):
     """
@@ -272,7 +268,7 @@ def upload_post(file: bytes, file_ext: str, metadata: dict = None, file_path: st
             logger.debug(
                 f'File "{file_path} is too similar to post {entry["post"]["id"]} ({100 - entry["distance"]}%)',
             )
-            post.exact_post = True
+            post.exact_post = entry
             break
 
     if not post.exact_post:
@@ -299,21 +295,17 @@ def upload_post(file: bytes, file_ext: str, metadata: dict = None, file_path: st
         # Tag post if enabled
         if config.upload_media['auto_tag']:
             saucenao_limit_reached = auto_tagger(str(post_id), post.media, saucenao_limit_reached, original_md5)
-    
+
     #If the exact post was found in szurubooru
     else:
-        if True:# change to optional flag for append tags
-            post.exact_post.tags = list(set().union(post.exact_post.tags, post.tags))
-            
-        if True:# change to optional flag for swap to higher res
-            post_resolution = get_image_resolution(post.media)
-            exact_post_resolution = get_image_resolution(post.exact_post.media)
+        if True and metadata['tags']:# change True to optional flag for append tags
+            tags = str(', '.join(metadata['tags']))
+            id = str(post.exact_post['id']) if 'id' in post.exact_post else str(post.exact_post['post']['id'])
+            subprocess.run(
+                ['tag-posts', '--mode', 'append', '--add-tags', tags, id],
+                check=True,
+            )
 
-            if post_resolution > exact_post_resolution:
-                post.exact_post.media = post.media
-
-                
-    
     return True, saucenao_limit_reached
 
 
