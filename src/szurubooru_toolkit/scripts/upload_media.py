e5ae595fa1144f5f2666fc00bed97b80d227623c
reluce <reluce@fkosquad.moe>
2024-03-03 12:01:56 +0100

    add option to update tags if post is alreay uploaded

--- src/szurubooru_toolkit/scripts/upload_media.py
+++ src/szurubooru_toolkit/scripts/upload_media.py
@@ -13,6 +13,7 @@ from tqdm import tqdm
 from szurubooru_toolkit import config
 from szurubooru_toolkit import szuru
 from szurubooru_toolkit.scripts import auto_tagger
+from szurubooru_toolkit.scripts import tag_posts
 from szurubooru_toolkit.szurubooru import Post
 from szurubooru_toolkit.szurubooru import Szurubooru
 from szurubooru_toolkit.utils import get_md5sum
@@ -193,7 +194,7 @@ def cleanup_dirs(dir: str) -> None:
                 pass
 
 
-def eval_convert_image(file: bytes, file_ext: str, file_to_upload: str = None) -> tuple(bytes | str):
+def eval_convert_image(file: bytes, file_ext: str, file_to_upload: str = None) -> tuple[bytes | str]:
     """
     Evaluate if the image should be converted or shrunk and if so, do so.
 
@@ -333,6 +334,13 @@ def upload_post(
                 limit_reached=saucenao_limit_reached,
                 md5=original_md5,
             )
+
+    # If the exact post was found in szurubooru
+    else:
+        if config.import_from_url['update_tags_if_exists'] and metadata and metadata['tags']:
+            id = str(post.exact_post['id']) if 'id' in post.exact_post else str(post.exact_post['post']['id'])
+            config.tag_posts['mode'] = 'append'
+            tag_posts.main(query=id, add_tags=metadata['tags'])
 
     return True, saucenao_limit_reached
 
